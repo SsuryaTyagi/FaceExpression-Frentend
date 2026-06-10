f83b242eec74cbb02669d7ce0d9f2a85e1f2b2a4
SsuryaTyagi <2040surya@gmail.com>
2026-06-10 13:19:33 +0530

    github auth setup

--- src/features/auth/component/GithubButton.jsx
+++ src/features/auth/component/GithubButton.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { VscGithub } from "react-icons/vsc";
 
+const githubLoginURL = `http://localhost:3000/github`;
+
 export default function GithubButton() {
   return (
-    <button className="auth__social-btn">
+    <button className="auth__social-btn"
+     onClick={() => window.location.href = githubLoginURL}
+    >
       <VscGithub size={16} /> GitHub
     </button>
   );
