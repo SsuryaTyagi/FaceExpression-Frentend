2ae5bfc211cff7d9bb314ec1092dc4bd30feff66
SsuryaTyagi <2040surya@gmail.com>
2026-06-24 13:58:09 +0530

    vercel 8

--- src/features/auth/component/VerifyEmailPage.jsx
+++ src/features/auth/component/VerifyEmailPage.jsx
@@ -8,7 +8,7 @@ export default function VerifyEmailPage() {
   const [status, setStatus] = useState("verifying");
 
   useEffect(() => {
-    axios.get(`http://localhost:3000/verify-email/${token}`)
+    axios.get(`face-expression-backend.vercel.app/verify-email/${token}`)
       .then(() => { setStatus("success"); setTimeout(() => navigate("/login"), 2000); })
       .catch((err) => setStatus(err.response?.data?.message || "Invalid link"));
   }, [token]);
